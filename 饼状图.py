da790e4ddf8efb193820708ae0c2854c8640d315
不伦不类 <zly9944@126.com>
2022-08-17 10:00:32 +0800

    V14

--- 饼状图.py
+++ 饼状图.py
@@ -1,8 +1,9 @@
-import  pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 df = pd.read_excel('./data/tongji.xlsx',index_col='寄件人')
 print(df)
+print(d.index)
 
 df['总邮资'].plot.pie(fontsize=8,counterclock=False,startangle=-270)
 plt.rcParams['font.sans-serif'] = ['SimHei']
